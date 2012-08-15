3f9d9dc83d27fdedde47351b5032d43df80fdebe
Nathaniel Husted <nhusted@indiana.edu>
2012-08-15 09:45:20 -0700

    Added functionality to the getID btn to currently display the userID, gID, and groups.

--- src/com/example/auditpermissionexample/MainActivity.java
+++ src/com/example/auditpermissionexample/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.auditpermissionexample;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -20,7 +25,28 @@ public class MainActivity extends Activity {
     }
     
     public void btnGetID_onClick(View view) {
-    
+    	Process getIds = null;
+    	
+    	try {
+    		getIds = new ProcessBuilder()
+    					.command("id")
+    					.redirectErrorStream(true)
+    					.start();
+    		
+    		BufferedReader in = new BufferedReader(new InputStreamReader(getIds.getInputStream()));
+   		 
+    		String line = "";
+    		
+    		while((line = in.readLine()) != null) {
+    			TextView txtOutput = (TextView) findViewById(R.id.txtViewOutput);
+    			txtOutput.setText(line + "\n");
+    		}
+    		
+    	}
+    	catch (IOException e) {
+    		e.printStackTrace();
+    		getIds.destroy();
+    	}    
     }
     
     public void toggleBtnAuditStream_onClick(View view) {
